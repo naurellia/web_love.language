2e0865ef8424da17c3d27127db514463b02f5db3
naurellia <p22-nayla553@smkn7-smr.sch.id>
2024-09-13 07:42:23 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        kuning:'rgb(255,176,46)',
+        abu:'rgb(84,84,84)',
+      }
+    },
+  },
+  plugins: [],
+}
